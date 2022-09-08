d1765dc9c9e82f62fd42eb710f5af34e9bf96515
Nick Wells <wells.nicholas@gmail.com>
2022-09-08 13:57:15 +0100

    refactor: minor changes to keep gofumpt quiet

--- bankACAnalysis/doc.go
+++ bankACAnalysis/doc.go
@@ -1,8 +1,6 @@
 /*
-
 bankACAnalysis will take a file of transactions (comma-separated values) and
 arrange them into a tree of types such as entertainment, food, utility bills
 etc.
-
 */
 package main
