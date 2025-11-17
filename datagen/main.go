dc21f4dcea2a5b2a634d57ddf8fdd37d7d4f7eb0
Nick Wells <wells.nicholas@gmail.com>
2025-11-17 11:32:11 +0000

    refactor(datagen): use newer package features

--- datagen/main.go
+++ datagen/main.go
@@ -5,6 +5,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
+	"os"
 	"strings"
 	"time"
 
@@ -15,11 +17,6 @@ import (
 )
 
 // Created: Sat Aug 20 12:19:22 2022
-
-var startTime = time.Date(
-	2022, time.August, 25,
-	13, 0, 0, 0,
-	time.FixedZone("UTC", 0))
 
 // prog holds the parameters and current status of the program
 type prog struct {
@@ -35,32 +32,147 @@ func newProg() *prog {
 	}
 }
 
+// intermittentIterator replaces the Next function on a Generator with one
+// that is only called intermittently.
+type intermittentIterator[T any] struct {
+	next int
+	min  int
+	max  int
+}
+
+// Next ...
+func (ti *intermittentIterator[T]) Next(tg datagen.TypedGenerator[T]) {
+	if ti.next == 0 {
+		ti.next = ti.min + rand.IntN(ti.max-ti.min) //nolint:gosec
+	}
+
+	ti.next--
+	if ti.next == 0 {
+		tg.Next()
+	}
+}
+
+// makeTimeGen creates and returns a generator for the date
+func makeTimeGen() datagen.TypedGenerator[time.Time] {
+	interIter := &intermittentIterator[time.Time]{
+		min: 3,
+		max: 8,
+	}
+	startTime := time.Date(2025, time.June, 1, 12, 0, 0, 0,
+		time.FixedZone("UTC", 0))
+
+	timeGen, err := datagen.NewGenWrapper[time.Time](datagen.NewTimeGen(
+		datagen.TimeGenSetLayout("Mon 02/01/2006"),
+		datagen.TimeGenSetInitialTime(startTime),
+		datagen.TimeGenSetIntervalF(
+			datagen.TimeGenConstIntervalF(time.Hour*24)),
+	),
+		datagen.GenWrapperSetIterator[time.Time](interIter),
+	)
+	if err != nil {
+		fmt.Println("Cannot build the GenWrapper", err)
+		os.Exit(1)
+	}
+
+	return timeGen
+}
+
+// sinceLast satisfies the datagen.Passer interface. Its Passes method checks
+// that the supplied TypedVal[time.Time] is more than the given time since
+// the last time it passed.
+type sinceLast struct {
+	val      datagen.TypedVal[time.Time]
+	last     time.Time
+	interval time.Duration
+}
+
+// Passes returns true if the time falls on the first Monday of the month and
+// the embedded TypedVal[time.Time] is more that 'interval' since 'last'. It
+// assumes that the time values returned by val.Value() are increasing.
+func (sl *sinceLast) Passes() bool {
+	if sl.val.Value().Sub(sl.last) > sl.interval {
+		sl.last = sl.val.Value()
+
+		return true
+	}
+
+	return false
+}
+
+// makeSalaryCheck returns a Passer that will pass if the record should be a
+// Salary record. It is based on the date - if the date is the first Monday
+// of the month and this is the first time we've seen it in the last 2 days.
+func makeSalaryCheck(timeVal datagen.TypedVal[time.Time]) datagen.Passer {
+	return datagen.MakeAndPasser(
+		datagen.MakePasser(
+			check.TimeIsNthWeekdayOfMonth(1, time.Monday),
+			timeVal),
+		&sinceLast{
+			interval: time.Hour * 24 * 2, // two days
+			val:      timeVal,
+		},
+	)
+}
+
+// salaryGenImpl has a SetVal method thereby satisfying the ValSetter
+// interface
+type salaryGenImpl struct {
+	p datagen.Passer
+}
+
+// SetVal ...
+func (sgi salaryGenImpl) SetVal(v *bool) {
+	*v = sgi.p.Passes()
+}
+
 func main() {
 	prog := newProg()
 	ps := makeParamSet(prog)
 
 	ps.Parse()
 
-	sortCodeGen := datagen.NewGen(datagen.GenSetValue("'12-34-56"))
+	timeGen := makeTimeGen()
+	isSalary := datagen.NewGen(
+		datagen.GenSetValue(false),
+		datagen.GenSetValSetter[bool](
+			salaryGenImpl{
+				p: makeSalaryCheck(timeGen),
+			}))
+	salaryCheck := datagen.MakeBoolPasser(isSalary)
+	sortCodeGen := datagen.NewGen(datagen.GenSetValue("12-34-56"))
 	acctNumGen := datagen.NewGen(datagen.GenSetValue("09876543"))
 
-	transTypeGen := datagen.NewWStringGen(datagen.Random,
-		datagen.WeightedString{Str: "BGC", Weight: 1},
-		datagen.WeightedString{Str: "SO", Weight: 8},
-		datagen.WeightedString{Str: "DD", Weight: 20},
-		datagen.WeightedString{Str: "DEB", Weight: 97},
+	nonSalaryTransTypeGen := datagen.NewWStringGen(datagen.Random,
+		datagen.WeightedString{Str: "DEB", Weight: 10},
+		datagen.WeightedString{Str: "DD", Weight: 1},
 	)
-
+	transTypeGen := datagen.NewSwitchGen(
+		nonSalaryTransTypeGen, // default
+		datagen.NewCase(salaryCheck, datagen.NewGen(datagen.GenSetValue("BGC"))))
+	directDebitCheck := datagen.MakePasser(check.ValEQ("DD"), transTypeGen)
 	debitDescGen := datagen.NewWStringGen(datagen.Random,
 		datagen.WeightedString{Str: "LOCAL CAFE", Weight: 3},
 		datagen.WeightedString{Str: "LOCAL SUPERMARKET", Weight: 5},
 		datagen.WeightedString{Str: "POSH SUPERMARKET", Weight: 1},
 		datagen.WeightedString{Str: "CHEAP SUPERMARKET", Weight: 3},
+		datagen.WeightedString{Str: "PUB", Weight: 4},
+		datagen.WeightedString{Str: "GASTROPUB", Weight: 2},
+		datagen.WeightedString{Str: "FANCY RESTAURANT", Weight: 1},
+		datagen.WeightedString{Str: "BOOKSHOP", Weight: 1},
 	)
-	creditDescGen := datagen.NewGen(datagen.GenSetValue("EMPLOYER"))
+	directDebitDescGen := datagen.NewWStringGen(datagen.Random,
+		datagen.WeightedString{Str: "COUNCIL", Weight: 3},
+		datagen.WeightedString{Str: "PHONE COMPANY", Weight: 5},
+		datagen.WeightedString{Str: "GAS AND ELEC", Weight: 1},
+		datagen.WeightedString{Str: "WATER COMPANY", Weight: 3},
+	)
+	transDescGen := datagen.NewSwitchGen(
+		debitDescGen, // default
+		datagen.NewCase(directDebitCheck, directDebitDescGen),
+		datagen.NewCase(salaryCheck, datagen.NewGen(datagen.GenSetValue("EMPLOYER"))),
+	)
 
-	transIsBGC := datagen.NewValCk(check.ValEQ("BGC"), transTypeGen)
-	transIsNotBGC := datagen.NewValCk(
+	transIsNotBGC := datagen.MakePasser(
 		check.Not(check.ValEQ("BGC"), ""),
 		transTypeGen)
 
@@ -89,7 +201,7 @@ func main() {
 	debitGen := datagen.NewSwitchGen(zeroGen,
 		datagen.NewCase(transIsNotBGC, randMoneyGen))
 	creditGen := datagen.NewSwitchGen(zeroGen,
-		datagen.NewCase(transIsBGC, salaryGen))
+		datagen.NewCase(salaryCheck, salaryGen))
 	aggregator := func(v *datagen.Money,
 		vals ...datagen.TypedGenerator[datagen.Money],
 	) {
@@ -101,22 +213,21 @@ func main() {
 			datagen.NewComputedValSetter(
 				aggregator, debitGen, creditGen)))
 
-	r := datagen.NewRecord("",
-		datagen.NewField("Transaction Date",
-			datagen.NewTimeGen(
-				datagen.TimeGenSetLayout("02/01/2006"),
-				datagen.TimeGenSetInitialTime(startTime))),
+	r, err := datagen.NewRecord("",
+		datagen.NewField("Transaction Date", timeGen),
+		datagen.NewHiddenField(isSalary),
 		datagen.NewField("Transaction Type", transTypeGen),
 		datagen.NewField("Sort Code", sortCodeGen),
 		datagen.NewField("Account Number", acctNumGen),
-		datagen.NewField("Transaction Description",
-			datagen.NewSwitchGen(
-				debitDescGen,
-				datagen.NewCase(transIsBGC, creditDescGen))),
+		datagen.NewField("Transaction Description", transDescGen),
 		datagen.NewField("Debit Amount", debitGen),
 		datagen.NewField("Credit Amount", creditGen),
 		datagen.NewField("Balance", balanceGen),
 	)
+	if err != nil {
+		fmt.Println("cannot build the report:", err)
+		os.Exit(1)
+	}
 
 	fmt.Println(strings.Join(r.GenerateTitles(), ","))
 
